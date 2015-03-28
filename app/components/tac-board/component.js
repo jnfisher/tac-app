9f5714b2207680b05ca9fafb556bb5a14d0c040e
John Fisher <john.fisher@atomicobject.com>
2015-03-28 12:32:20 -0400

    Implement winning states, etc.

--- app/components/tac-board/component.js
+++ app/components/tac-board/component.js
@@ -4,30 +4,60 @@ import layout from './template';
 export default Ember.Component.extend({
   layout: layout,
   currentTurn: 0,
+  winningStates: [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
 
-  button0State: '-',
-  button1State: '-',
-  button2State: '-',
-  button3State: '-',
-  button4State: '-',
-  button5State: '-',
-  button6State: '-',
-  button7State: '-',
-  button8State: '-',
+  buttonStates: Ember.A([
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'}),
+    Ember.Object.create({ value: '-'})]),
 
   clearStates: function() {
-    for (var i=0; i < 9; i++) {
-      this.set("button"+i+"State", '-')
+    for (var i=0; i < this.get('buttonStates.length'); i++) {
+      this.set('buttonStates.'+i+'.value', '-');
     }
   },
+
+  gameIsFinished: function() {
+    return (this.get('gameWinner') != null);
+  }.property('gameWinner'),
+
+  checkWinner: function(player) {
+    for (var i=0; i < this.get('winningStates').length; i++) {
+      if (this.get('buttonStates.'+this.get('winningStates')[i][0]+'.value') === player &&
+          this.get('buttonStates.'+this.get('winningStates')[i][1]+'.value') === player &&
+          this.get('buttonStates.'+this.get('winningStates')[i][2]+'.value') === player) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  gameWinner: function() {
+    console.log('checking winner...');
+
+    if (this.checkWinner("X"))
+      return "X"
+    else if (this.checkWinner("O"))
+      return "O"
+    else
+      return null;
+  }.property('buttonStates.@each.value'),
 
   actions: {
     buttonClicked: function(turnUsed) {
       if(turnUsed) {
-        this.set('currentTurn', (this.get('currentTurn') + 1)%2 );
-        console.log(this.get('currentTurn'));
+        this.set('currentTurn', (this.get('currentTurn') + 1) % 2 );
       }
-      // Ember.run.next(this, "clearStates")
+    },
+
+    clearClicked: function() {
+      Ember.run.next(this, "clearStates");
     }
   }
 });
